646ef4d341b30ea7e286d53ae0f104782b3b934e
AdamJaneczech <anndyjan@seznam.cz>
2022-08-05 10:37:51 +0200

    async-lib-test

---
+++ src/Config.hpp
@@ -0,0 +1,26 @@
+static String response = "HTTP/1.1 200 OK   \
+Content-type:text/html  \
+Connection: close";
+
+static String ConfigPage = 
+
+"<!DOCTYPE html> \
+<html lang=\"en\">  \
+<head>  \
+    <meta charset=\"UTF-8\">    \
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">   \
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">  \
+    <title>ESP32 FLUNI ticker</title>   \
+</head> \
+<body>  \
+    <form action=\"/url\" method=\"GET\">   \
+        <p>Enter the network SSID and password below:</p>    \
+        <input type=\"text\" placeholder=\"SSID\">  \
+        <input type=\"text\" placeholder=\"Password\" minlength=\"8\">  \
+        <button type=\"submit\">Submit</button> \
+    </form> \
+</body> \
+</html>";
+
+const char* input_ssid = "ssid";
+const char* input_password = "password";
